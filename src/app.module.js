183021736e6d8b58ef2dd613f0fc7643351770dd
mathoks <mathoks2012@gmail.com>
2024-10-12 16:18:07 +0100

    my first

--- src/app.module.js
+++ src/app.module.js
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { ProductsController } from './products/products.controller';
+import { ProductsService } from './products/products.service';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UsersController, ProductsController],
+  providers: [AppService, ProductsService, UsersService],
 })
 export class AppModule {}
